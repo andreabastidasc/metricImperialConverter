68a2f26a96e3d884a499958b307c8b0b442419ef
Andrea Bastidas <andrea.bcolina@gmail.com>
2022-11-23 16:42:09 -0300

    feat: unit validations and res json

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -37,9 +37,24 @@ function ConvertHandler() {
   };
   
   this.getUnit = function(input) {
-    let result = splitter(input)[1];
-    
-    return result;
+    let result = splitter(input)[1].toLowerCase();
+
+    switch (result) {
+      case 'km':
+        return 'km';
+      case 'gal':
+        return 'gal';
+      case 'lbs': 
+        return 'lbs';
+      case 'mi':
+        return 'mi';
+      case 'l':
+        return 'L';
+      case 'kg':
+        return 'kg';
+      default:
+        return undefined;
+    }
   };
   
   this.getReturnUnit = function(initUnit) {
